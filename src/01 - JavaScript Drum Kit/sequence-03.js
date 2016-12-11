ecb7c96f2cb8a9b955181347e4a60a1e7e3893fc
Julio Makdisse Saito <saitodisse@gmail.com>
2016-12-11 19:50:00 -0200

    01: sequence-03 refactor

--- src/01 - JavaScript Drum Kit/sequence-03.js
+++ src/01 - JavaScript Drum Kit/sequence-03.js
@@ -1,4 +1,4 @@
-/* global AudioContext, XMLHttpRequest, requestAnimationFrame */
+/* global AudioContext, XMLHttpRequest */
 
 function start () {
   window.AudioContext = window.AudioContext || window.webkitAudioContext
@@ -20,60 +20,53 @@ function start () {
     })
   }
 
+  function playSound (buffer, time) {
+    const source = context.createBufferSource()
+    source.buffer = buffer
+    source.connect(context.destination)
+    if (!source.start) {
+      source.start = source.noteOn
+    }
+    source.start(time)
+  }
+
   let hithatBuffer
   let kickBuffer
   let snareBuffer
 
-  loadAudio('sounds/hihat.wav').then((buffer) => {
-    hithatBuffer = buffer
-  })
-    .then(() => {
-      return loadAudio('sounds/kick.wav').then((buffer) => {
-        kickBuffer = buffer
-      })
-    })
-    .then(() => {
-      return loadAudio('sounds/snare.wav').then((buffer) => {
-        snareBuffer = buffer
-      })
-    })
-    .then(() => {
-      function playSound (buffer, time) {
-        const source = context.createBufferSource()
-        source.buffer = buffer
-        source.connect(context.destination)
-        if (!source.start) {
-          source.start = source.noteOn
-        }
-        source.start(time)
+  function load () {
+    return Promise.all([
+      loadAudio('sounds/hihat.wav').then((buffer) => { hithatBuffer = buffer }),
+      loadAudio('sounds/kick.wav').then((buffer) => { kickBuffer = buffer }),
+      loadAudio('sounds/snare.wav').then((buffer) => { snareBuffer = buffer })
+    ])
+  }
+
+  let play = () => {
+    // We'll start playing the rhythm 100 milliseconds from "now"
+    const startTime = context.currentTime
+    const tempo = 120 // BPM (beats per minute)
+    const eighthNoteTime = (60 / tempo) / 2
+
+    // Play 2 bars of the following:
+    for (let bar = 0; bar < 2; bar++) {
+      const time = startTime + bar * 8 * eighthNoteTime
+      // Play the bass (kick) drum on beats 1, 5
+      playSound(kickBuffer, time)
+      playSound(kickBuffer, time + 4 * eighthNoteTime)
+
+      // Play the snare drum on beats 3, 7
+      playSound(snareBuffer, time + 2 * eighthNoteTime)
+      playSound(snareBuffer, time + 6 * eighthNoteTime)
+
+      // Play the hi-hat every eighthh note.
+      for (let i = 0; i < 8; ++i) {
+        playSound(hithatBuffer, time + i * eighthNoteTime)
       }
+    }
+  }
 
-      const kick = kickBuffer
-      const snare = snareBuffer
-      const hihat = hithatBuffer
-
-      // We'll start playing the rhythm 100 milliseconds from "now"
-      const startTime = context.currentTime
-      const tempo = 120 // BPM (beats per minute)
-      const eighthNoteTime = (60 / tempo) / 2
-
-      // Play 2 bars of the following:
-      for (let bar = 0; bar < 2; bar++) {
-        const time = startTime + bar * 8 * eighthNoteTime
-        // Play the bass (kick) drum on beats 1, 5
-        playSound(kick, time)
-        playSound(kick, time + 4 * eighthNoteTime)
-
-        // Play the snare drum on beats 3, 7
-        playSound(snare, time + 2 * eighthNoteTime)
-        playSound(snare, time + 6 * eighthNoteTime)
-
-        // Play the hi-hat every eighthh note.
-        for (let i = 0; i < 8; ++i) {
-          playSound(hihat, time + i * eighthNoteTime)
-        }
-      }
-    })
+  load().then(play)
 }
 
 window.onload = start
